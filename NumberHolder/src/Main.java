67f0577c4e5cf5e3ef14c26b7da7e4b9888b0370
OlexandrYatskiv <99140255+OlexandrYatskiv@users.noreply.github.com>
2023-01-18 21:46:42 +0300

    Add files via upload

---
+++ NumberHolder/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+        NumberHolder first = new NumberHolder();
+        first.anInt = 2454;
+        first.aFloat = 8.2f;
+        System.out.println("integer value: " + first.anInt);
+        System.out.println("float value: " + first.aFloat);
+
+
+
+
+
+
+
+
+  }
+
+}
---
+++ NumberHolder/src/NumberHolder.java
@@ -0,0 +1,6 @@
+public class NumberHolder {
+
+        public int anInt;
+        public float aFloat;
+}
+
